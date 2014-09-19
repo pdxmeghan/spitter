d72fab874e3a57d43ed1f7dd65ee1d680d7510b5
Josie Wright & Meghan Lindsley <student@epicodus.com>
2014-09-19 09:51:20 -0700

    add scope for uniqueness of follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,6 +3,7 @@ class Follow < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :follower_id, :presence => true
 
+  validates :user_id, uniqueness: { :scope => :follower_id }
 
   belongs_to :user, :dependent => :destroy
   belongs_to :followed_users, class_name: 'User'
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -5,6 +5,8 @@ describe Follow do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :follower_id }
 
+  it {should validate_uniqueness_of(:user_id).scoped_to(:follower_id)}
+
   it { should belong_to :user }
 
 end
