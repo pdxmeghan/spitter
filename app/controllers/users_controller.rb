a7ffcbcad887bd2e87ff8064f7ce1e74888d78b4
Meghan Lindsley & Josie Wright <student@epicodus.com>
2014-09-22 10:14:00 -0700

    repopulate spits list upon creation of new spit. refactor dropdown menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user), notice: "Thanks for signing up!"
+      redirect_to users_path, notice: "Thanks for signing up!"
     else
       render 'new'
     end
